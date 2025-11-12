4ba3ef787e66fc099bb4b664a17669dab959fd7f
Utkarsh9june <utkarshgoyal648@gmail.com>
2025-11-12 23:58:52 +0530

    Added Dashboard API fetch

--- client/src/components/layouts/SideMenu.jsx
+++ client/src/components/layouts/SideMenu.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { SIDE_MENU_DATA } from './../../utils/data';
 import { UserContext } from '../../context/userContext';
 import { useNavigate } from 'react-router-dom';
+import CharAvatar from '../Cards/CharAvatar';
 
 const SideMenu = ({activeMenu}) => {
   const {user, clearUser} = useContext(UserContext);
@@ -20,9 +21,9 @@ const SideMenu = ({activeMenu}) => {
     navigate("/login");
   }
   return (
-    <div className='w-64 h-100vh bg-white border-r border-gray-200/50 p-5 sticky left-0 z-20'>
+    <div className='w-64 h-100vh bg-white border-r border-gray-200/50 px-5 sticky left-0 z-20'>
       <div className='flex flex-col items-center justify-center gap-3 mt-3 mb-7'>
-        {user?.profileImageUrl ? (<img src={user?.profileImageUrl || ""} alt='Profile Image' className='w-20 h-20 bg-slate-400 rounded-full' />) : <></>}
+        {user?.profileImageUrl ? (<img src={user?.profileImageUrl || ""} alt='Profile Image' className='w-20 h-20 bg-slate-400 rounded-full' />) : <CharAvatar fullName={user?.fullName} width="w-20" height="h-20" style="text-2xl" />}
         <h5 className='font-medium leading-6'>{user?.fullName || ""}</h5>
       </div>
       {SIDE_MENU_DATA.map((item,index) => (
@@ -36,3 +37,4 @@ const SideMenu = ({activeMenu}) => {
 }
 
 export default SideMenu
+ 
--- client/src/pages/Dashboard/Home.jsx
+++ client/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useUserAuth } from './../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const Home = () => {
+  useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashBoardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if(loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(`${API_PATHS.DASHBOARD.GET_DATA}`);
+      if(response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Something went wrong.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className='my-5 mx-auto'></div>
