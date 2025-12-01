56171cab0fd7b646613e82f88d2d3ae0c466334c
Utkarsh9june <utkarshgoyal648@gmail.com>
2025-12-01 18:30:26 +0530

    Added changes

--- client/src/pages/Dashboard/Income.jsx
+++ client/src/pages/Dashboard/Income.jsx
@@ -3,6 +3,9 @@ import DashboardLayout from '../../components/layouts/DashboardLayout'
 import IncomeOverview from '../../components/Income/IncomeOverview';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import Modal from '../../components/Modal';
+import AddIncomeForm from '../../components/Income/AddIncomeForm';
+import toast from 'react-hot-toast';
 
 const Income = () => {
   const [incomeData, setIncomeData] = useState([]);
@@ -11,7 +14,7 @@ const Income = () => {
     show:false,
     data:null,
   });
-  const [openAddIncomeModel, setOpenAddIncomeModel] = useState(false);
+  const [openAddIncomeModel, setOpenAddIncomeModel] = useState(true);
 
   const fetchIncomeDetails = async () => {
     if(loading) return;
@@ -28,6 +31,34 @@ const Income = () => {
     }
   };
 
+  const handleAddIncome = async (income) => {
+    const {source, amount, date, icon} = income;
+
+    if(!source.trim()) {
+      toast.error("Source is required.");
+      return;
+    }
+
+    if(!amount || isNaN(amount) || Number(amount)<=0) {
+      toast.error("Amount should be a number greater than 0");
+      return;
+    }
+
+    if(!date) {
+      toast.error("Date is required");
+      return;
+    }
+
+    try {
+      await axiosInstance.post(API_PATHS.INCOME.ADD_INCOME, {source, amount, date, icon});
+      setOpenAddIncomeModel(false);
+      toast.success("Income added successfully");
+      fetchIncomeDetails();
+    } catch(error) {
+      console.error("Error adding income", error.response?.data?.message || error.message);
+    }
+  };
+
   useEffect(() => {
     fetchIncomeDetails();
     return () => {};
@@ -41,6 +72,11 @@ const Income = () => {
             <IncomeOverview transactions={incomeData} onAddIncome={() => setOpenAddIncomeModel(true)} />
           </div>
         </div>
+        <Modal isOpen={openAddIncomeModel} onClose={() => setOpenAddIncomeModel(false)} title="Add Income">
+          <div>
+            <AddIncomeForm onAddIncome = {handleAddIncome} />
+          </div>
+        </Modal>
       </div>
     </DashboardLayout>
   )
